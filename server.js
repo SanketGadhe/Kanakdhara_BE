1ae851db69673e8f5e3b1ed09612f1e29c5a32ef
SanketGadhe <sanketgadhe366@gmail.com>
2025-12-27 02:47:25 +0530

    trust prxy

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const connectDB = require("./config/db");
 const app = express();
 
 // Trust proxy for deployment platforms (Render, Heroku, etc.)
-app.set('trust proxy', 1);
+// app.set('trust proxy', 1);
 
 // Security middleware
 const rateLimit = require("express-rate-limit");
